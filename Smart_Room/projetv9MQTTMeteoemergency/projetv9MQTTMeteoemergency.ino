166878885c4f874c67121cfc97fe3d3585c8c492
peterthemercenary <pierrediamanesenghor@esp.sn>
2024-12-04 10:09:15 +0100

    mqtt complet

--- Smart_Room/projetv9MQTTMeteoemergency/projetv9MQTTMeteoemergency.ino
+++ Smart_Room/projetv9MQTTMeteoemergency/projetv9MQTTMeteoemergency.ino
@@ -67,7 +67,7 @@ void messageCallback(char* topic, uint8_t* payload, unsigned int length) {
 
     Serial.print("Message : ");
     Serial.println(message);
-
+    screen.show(0,255,255,message,"");
     // Exemple : si le message est "1", on active l'alarme
     if (message == "1") {
         AlarmActivated = false;
