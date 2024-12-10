ab7258a2becc4d43ed8c6e02a59ed6fc8e793fd1
peterthemercenary <pierrediamanesenghor@esp.sn>
2024-12-10 23:16:35 +0100

    the end

---
+++ Smart_Airport/credentials.cpp
@@ -0,0 +1,12 @@
+#include "credentials.hpp"
+
+// Accès Wi-Fi
+const char* WiFi_ssid = "Wifizinho";      // Nom du Wi-Fi
+const char* WiFi_Password = "Senha123";   // Mot de passe du Wi-Fi
+// Accès MQTT
+const char* mqttServer = "x51fc22b.ala.dedicated.aws.emqxcloud.com";
+const int mqttPort = 1883;
+const char* mqttUser = "pierrele123";
+const char* mqttPassword = "projetairport";
+
+// Adresse IP: 192.168.137.10
